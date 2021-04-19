505c25677a66257f007ec5056ef3447f51a186a5
Ava Thorn <avatheavian@gmail.com>
2021-04-19 01:45:22 -0400

    Fixed deepsource issues

--- tests/conftest.py
+++ tests/conftest.py
@@ -4,11 +4,11 @@ import typing
 
 import pytest
 
-from util import mock_secrets
+from tests.util import mock_secrets
 
 
 @pytest.fixture(autouse=True)
 def setup_fixture() -> typing.Generator:
-    with patch('webexteamssdk.WebexTeamsAPI') as mock_webex:
-        with patch("builtins.open", mock_open(read_data=json.dumps(mock_secrets))):
-            yield mock_webex
+    with patch('webexteamssdk.WebexTeamsAPI') as mock_webex, \
+            patch("builtins.open", mock_open(read_data=json.dumps(mock_secrets))):
+        yield mock_webex
